7b2c3c71ddebf88bbe68ac1a9bf9634a1db57231
Raj <rajsidwadkar777@gmail.com>
2026-02-15 16:56:11 +0530

    movezerostoend2

--- movezerostoend.py
+++ movezerostoend.py
@@ -1,19 +1,33 @@
 def movezerostoend(arr):
-    
-    n = len(arr)
-    result = [0] * n
 
-    i = j = 0
+    n = len(arr)
+    
+    # SC = O(N)
+    # result = [0] * n
+    # j = 0
+    # for i in range(n):
+    #     if arr[i] != 0:
+    #         result[j] = arr[i]
+    #         j += 1
+
+    # return result
+
+    # SC = O(1)
+    j = 0
     for i in range(n):
         if arr[i] != 0:
-            result[j] = arr[i]
+            arr[j], arr[i] = arr[i], arr[j]
             j += 1
 
-    return result
+    return arr
+
+
 
         
 
-print(movezerostoend([1,0,5,0,3,4,0,2]))
+print(movezerostoend([5,0,5,0,3,4,0,2]))
+
+
 
         
 
